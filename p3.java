7e8c6013d2295a0da4de2889a68b1dca83b6b4bf
Apurva Kumar <apurva7k@gmail.com>
2024-11-28 23:40:01 +0530

    first commit

---
+++ p3.java
@@ -0,0 +1,10 @@
+public class p3 {
+    public static void main (String[] args) {
+        int a = 10;
+        int b = 5;
+        int sum = a + b;
+        System.out.println(sum);
+        int calc = (a * b) / (a - b);
+        System.out.println(calc);
+    }
+}
---
+++ p5.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class p5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name: "); //Apurva Kumar
+        String fullname = sc.nextLine();
+        System.out.print(fullname); //Apurva Kumar
+    }
+}
