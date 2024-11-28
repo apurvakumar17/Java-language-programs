0b1feb6e8eea8c1a888c85229a0d42fc840a601b
apurvakumar17 <apurva7k@gmail.com>
2024-11-28 23:44:52 +0530

    file rearrangement

---
+++ Practice Programs/p2.java
@@ -0,0 +1,12 @@
+public class p2 {
+    public static void main (String[] args) {
+        System.out.print("*\n**\n***\n****\n*****");
+        
+        System.out.println("");
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+    }
+}
---
+++ Practice Programs/p8.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class p8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter 1st number: ");
+        int a = sc.nextInt();
+        System.out.print("Enter 2nd number: ");
+        int b = sc.nextInt();
+
+        if (a == b) {
+            System.out.print("Numbers are same");
+        } else {
+            if (a > b) {                                            //else if replacement
+                System.out.print("First number is greater");
+            } else {
+                System.out.print("Second number is greater");
+            }
+        }
+    }
+}
