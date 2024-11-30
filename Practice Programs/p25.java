0b189ed63bb074571af87c38d3880b280617d10b
apurvakumar17 <apurva7k@gmail.com>
2024-11-30 14:15:50 +0530

    functions

---
+++ Practice Programs/p25.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class p25 {
+    public static int printFact(int n) {
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact = fact * i;
+        }
+        return fact;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        System.out.println("Factorial of " + num + " is = " + printFact(num));
+        sc.close();
+    }
+}
---
+++ Practice Programs/p26.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class p26 {
+    public static void checkPrime(int a) {
+        int isprime = 0;
+        if (a <= 1) {
+            System.out.println(a + " is not a prime");
+            return; // Numbers <= 1 are not prime
+        }
+        for (int i = 2; i <= a / 2; i++) {
+            if (a % i == 0) {
+                isprime = 1;
+                break;
+            }
+        }
+        if (isprime == 1) {
+            System.err.println(a + " is not prime");
+        } else {
+            System.out.println(a + " is a prime");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = 0;
+        int x = 10;
+        while (x >= 0) {
+            System.out.print("Enter a number: ");
+            num = sc.nextInt();
+            checkPrime(num);
+            x--;
+        }
+        sc.close();
+    }
+}
