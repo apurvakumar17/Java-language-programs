3a02c01fa5b3f7ac646307f2d770da45f6aa557f
apurvakumar17 <apurva7k@gmail.com>
2024-12-02 01:13:04 +0530

    1D and 2D arrays

---
+++ Practice Programs/p30.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class p30 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter rows of 2D array: ");
+        int rows = sc.nextInt();
+        System.out.print("Enter colums of 2D array: ");
+        int cols = sc.nextInt();
+        int[][] arr2d = new int[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print("Enter element (" + i + "," + j + ")"+": ");
+                arr2d[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < rows; i++) {
+            System.out.println(arr2d[i][0] + ", " + arr2d[i][1] + ", " + arr2d[i][2]);
+        }
+        sc.close();
+    }
+}
