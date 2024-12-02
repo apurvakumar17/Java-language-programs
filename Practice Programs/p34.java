7a15bb67c369bdea14d6befffaafe378552e9f4b
apurvakumar17 <apurva7k@gmail.com>
2024-12-02 14:26:31 +0530

    string concatenation

---
+++ Practice Programs/p34.java
@@ -0,0 +1,8 @@
+public class p34 {
+    public static void main(String[] args) {
+        String firstname = "Apurva ";
+        String lastname = "Kumar";
+        String fullname = firstname + lastname;
+        System.out.println(fullname);
+    }
+}
