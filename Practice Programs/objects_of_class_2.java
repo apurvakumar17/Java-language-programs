699d092b34600bb01c1bd70794c70ccc207e4b41
apurvakumar17 <apurva7k@gmail.com>
2025-02-18 21:36:35 +0530

    Update objects_of_class_2.java

--- Practice Programs/objects_of_class_2.java
+++ Practice Programs/objects_of_class_2.java
@@ -26,7 +26,7 @@ public class objects_of_class_2 {
     public static void main(String args[]) {
         Box myBox1 = new Box();
         Box myBox2 = new Box(5);
-        Box myBox3 = new Box(1.5, 2.5, 3.5);
+        Box myBox3 = new Box(1, 2, 3);
         System.out.println("Volume for no values given: " + myBox1.volume());
         System.out.println("Volumne for one side given: "+ myBox2.volume());
         System.out.println("Volume for l, b & h given: " + myBox3.volume());
