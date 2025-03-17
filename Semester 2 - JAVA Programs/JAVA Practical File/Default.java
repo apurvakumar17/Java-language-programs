09ba53ce45e7bdd76acec7ab63f0caedd0dbacf3
apurvakumar17 <apurva7k@gmail.com>
2025-03-17 19:16:42 +0530

    practical file completion started

---
+++ Semester 2 - JAVA Programs/JAVA Practical File/Default.java
@@ -0,0 +1,5 @@
+public class Default {
+    public static void main(String args[]) {
+        System.out.println("\nPractical 1, 04814902024, Apurva Kumar\n");
+    }
+}
---
+++ Semester 2 - JAVA Programs/JAVA Practical File/P1.java
@@ -0,0 +1,45 @@
+public class P1 {
+    public static void main(String[] args) {
+        System.out.println("Primitive Data Types in Java:");
+        
+        System.out.println("byte: ");
+        System.out.println("Size: " + Byte.BYTES + " bytes");
+        System.out.println("Range: " + Byte.MIN_VALUE + " to " + Byte.MAX_VALUE);
+        System.out.println();
+        
+        System.out.println("short: ");
+        System.out.println("Size: " + Short.BYTES + " bytes");
+        System.out.println("Range: " + Short.MIN_VALUE + " to " + Short.MAX_VALUE);
+        System.out.println();
+        
+        System.out.println("int: ");
+        System.out.println("Size: " + Integer.BYTES + " bytes");
+        System.out.println("Range: " + Integer.MIN_VALUE + " to " + Integer.MAX_VALUE);
+        System.out.println();
+        
+        System.out.println("long: ");
+        System.out.println("Size: " + Long.BYTES + " bytes");
+        System.out.println("Range: " + Long.MIN_VALUE + " to " + Long.MAX_VALUE);
+        System.out.println();
+        
+        System.out.println("float: ");
+        System.out.println("Size: " + Float.BYTES + " bytes");
+        System.out.println("Range: " + Float.MIN_VALUE + " to " + Float.MAX_VALUE);
+        System.out.println();
+        
+        System.out.println("double: ");
+        System.out.println("Size: " + Double.BYTES + " bytes");
+        System.out.println("Range: " + Double.MIN_VALUE + " to " + Double.MAX_VALUE);
+        System.out.println();
+        
+        System.out.println("char: ");
+        System.out.println("Size: " + Character.BYTES + " bytes");
+        System.out.println("Range: " + (int) Character.MIN_VALUE + " to " + (int) Character.MAX_VALUE);
+        System.out.println();
+        
+        System.out.println("boolean: ");
+        System.out.println("Size: 1 byte");
+        System.out.println("Range: " + Boolean.FALSE + " to " + Boolean.TRUE);
+        System.out.println("\nPractical 1, 04814902024, Apurva Kumar\n");
+    }
+}
